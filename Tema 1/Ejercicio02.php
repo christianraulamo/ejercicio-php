3421a7db205be606284cc620b00b0b287e351c60
christianraulamo <christianraulamo24@gmail.com>
2018-01-31 22:21:32 +0100

    Actualización de ejercicios

---
+++ Tema 1/Ejercicio02.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Mi numero es: 664237812<br>
+
+        <?php
+        echo 'Calle Gabriel y Galan Nº 8';
+        ?>
+    </body>
+</html>
