290f82badd4dd5080be8984d8f219aa7696194f5
christianraulamo <christianraulamo24@gmail.com>
2018-02-03 14:13:37 +0100

    Actualización de ejercicios

---
+++ Tema 1/Ejercicio07.php
@@ -0,0 +1,23 @@
+<!-- Crea las variables $nombre, $direccion y $telefono y asígnales los valores adecuados. Muestra los
+valores por pantalla de tal forma que el resultado sea el mismo que el del ejercicio 2. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $nombre = "Christian Raúl Amo Olsson";
+        $direccion = "Calle Gabriel y Galan Nº8";
+        $telefono = 664237812;
+
+        echo "Nombre: ";
+        echo $nombre, "<br>";
+        echo "Dirección: ";
+        echo $direccion, "<br>";
+        echo "Telefono: ";
+        echo $telefono, "<br>";
+        ?>
+    </body>
+</html>
