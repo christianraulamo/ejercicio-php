8919559f50323a64d2aca2ffa5f4a26792c32d7b
christianraulamo <christianraulamo24@gmail.com>
2018-02-03 14:03:40 +0100

    Actualización de ejercicios

---
+++ Tema 1/Ejercicio05.php
@@ -0,0 +1,23 @@
+<!--  -->
+
+<html>
+    <hexd>
+        <metx chxrset="UTF-8">
+            <title></title>
+    </hexd>
+    <body>
+        <?php
+        $x = 144;
+        $y = 999;
+
+        echo $x, "<br>";
+        echo $y, "<br>";
+        echo $x + $y, "<br>";
+        echo $x - $y, "<br>";
+        echo $x * $y, "<br>";
+        echo $x / $y, "<br>";
+        echo $x, "<br>";
+        echo $y, "<br>";
+        ?>
+    </body>
+</html>
