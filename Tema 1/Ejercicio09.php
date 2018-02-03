cab544eed715f58237ac60b289ccfb475e519934
christianraulamo <christianraulamo24@gmail.com>
2018-02-03 14:24:14 +0100

    Actualización de ejercicios

---
+++ Tema 1/Ejercicio09.php
@@ -0,0 +1,17 @@
+<!-- Realiza un conversor de pesetas a euros. La cantidad en pesetas que se quiere convertir deberá estar
+almacenada en una variable. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $pesetas = 3327.72;
+        
+        echo "3327.72 pesetas son: ";
+        echo $y = $pesetas / 166.386, " euros";
+        ?>
+    </body>
+</html>
