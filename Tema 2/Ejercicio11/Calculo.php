b0caada9049dbb2d601e4bafb0f1bfe8421169cd
christianraulamo <christianraulamo24@gmail.com>
2018-02-06 19:09:07 +0100

    Actualización de ejercicios

---
+++ Tema 2/Ejercicio11/Calculo.php
@@ -0,0 +1,15 @@
+<!-- Realiza un conversor de Kb a Mb. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $a = $_GET ['a'];
+
+        echo "$a Kb son ", $a / 1000, " Mb";
+        ?>
+    </body>
+</html>
---
+++ Tema 2/Ejercicio11/Index.php
@@ -0,0 +1,16 @@
+<!-- Realiza un conversor de Kb a Mb. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Conversor de Kb a Mb</h3>
+
+        <form action="Calculo.php" method="get">
+            Kb: <input type="number" name="a"><br>
+            <input type="submit" value="Conversor">
+        </form>
+    </body>
+</html>
