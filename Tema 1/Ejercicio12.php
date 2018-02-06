ab1b7ee1869b36500ebcd56e80a357d991b3b3c3
christianraulamo <christianraulamo24@gmail.com>
2018-02-06 12:27:07 +0100

    Actualización de ejercicios

---
+++ Tema 1/Ejercicio12.php
@@ -0,0 +1,22 @@
+<!-- Igual que el programa anterior, pero esta vez la pirámide debe aparecer invertida, con el vértice
+hacia abajo. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $asterisco = "*";
+
+        echo $asterisco, $asterisco, $asterisco, $asterisco, $asterisco, $asterisco, $asterisco, $asterisco, $asterisco, "<br>";
+        echo "&nbsp;" . "&nbsp;" . $asterisco . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco, "<br>";
+        echo "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco, "<br>";
+        echo "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco, "<br>";
+        echo "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco, "<br>";
+        echo "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco . "&nbsp;" . "&nbsp;" . $asterisco, "<br>";
+        echo "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . "&nbsp;" . $asterisco, "<br>";
+        ?>
+    </body>
+</html>
