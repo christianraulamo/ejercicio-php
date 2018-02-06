2aa9bcd7959cd04e11cbac7fae46184de8c89dc3
christianraulamo <christianraulamo24@gmail.com>
2018-02-06 13:30:38 +0100

    Actualización de ejercicios

---
+++ Tema 2/Ejercicio02/index.php
@@ -0,0 +1,16 @@
+<!-- Realiza un programa que pida dos números y luego muestre el resultado de su multiplicación. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h3>Calcula la multiplicación de dos números</h3>
+        
+        <form action="Calculo.php" method="get">
+            Primer número: <input type="number" name="a"><br>
+            Segundo número: <input type="number" name="b"><br>
+            <input type="submit" value="Multiplicar">
+        </form>
+    </body>
+</html>
