00d687b373bacce66e7789ef5a87c96a1bcb808f
christianraulamo <christianraulamo24@gmail.com>
2018-02-06 18:20:17 +0100

    Actualización de ejercicios

---
+++ Tema 2/Ejercicio05/Calculo.php
@@ -0,0 +1,16 @@
+<!-- Escribe un programa que calcule el área de un rectángulo. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $b = $_GET ['b'];
+        $a = $_GET['a'];
+        
+        echo "El area del rectangulo es: ", $a * $b, " metros cuadrados";
+        ?>
+    </body>
+</html>
---
+++ Tema 2/Ejercicio05/Index.php
@@ -0,0 +1,17 @@
+<!-- Escribe un programa que calcule el área de un rectángulo. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Calcula el area de un rectangulo</h3>
+
+        <form action="Calculo.php" method="get">
+            Longitud: <input type="number" name="b"><br>
+            Anchura: <input type="number" name="a"><br>
+            <input type="submit" value="Area">
+        </form>
+    </body>
+</html>
