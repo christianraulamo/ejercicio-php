733d303b410651d1577bb78bc23a050feef62193
christianraulamo <christianraulamo24@gmail.com>
2018-02-06 18:49:44 +0100

    Actualización de ejercicios

---
+++ Tema 2/Ejercicio07/Calculo.php
@@ -0,0 +1,15 @@
+<!-- Escribe un programa que calcule el total de una factura a partir de la base imponible. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $a = $_GET ['a'];
+
+        echo "La base total imponible es de: ", $a * 1.21;
+        ?>
+    </body>
+</html>
---
+++ Tema 2/Ejercicio07/Index.php
@@ -0,0 +1,16 @@
+<!-- Escribe un programa que calcule el total de una factura a partir de la base imponible. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Calcula total de la base imponible</h3>
+
+        <form action="Calculo.php" method="get">
+            Base: <input type="number" name="a"><br>
+            <input type="submit" value="Base">
+        </form>
+    </body>
+</html>
