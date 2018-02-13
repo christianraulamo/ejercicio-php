cf0bc6d9a5a740eb1189d43a74f5f0496fc01773
christianraulamo <christianraulamo24@gmail.com>
2018-02-13 13:35:43 +0100

    Actualización de ejercicios

---
+++ Tema3/Ejercicio04/Index.php
@@ -0,0 +1,18 @@
+<!-- Vamos a ampliar uno de los ejercicios de la relación anterior para considerar las horas extras. Escribe
+un programa que calcule el salario semanal de un trabajador teniendo en cuenta que las horas
+ordinarias (40 primeras horas de trabajo) se pagan a 12 euros la hora. A partir de la hora 41, se
+pagan a 16 euros la hora. -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Introduzca el numero de horas trabajadas a la semana para saber el salario que le corresponde</h3>
+        <form action="Salario.php" method="get">
+            Introduzca el numero de horas trabajadas: <input type="number" name="a"><br>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
